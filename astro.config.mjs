77723fda41b7882a5453101e7e3f30f0cb1715c0
88899 <githubmen@gmail.com>
2025-09-23 09:19:23 +0800

    index

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,17 +10,31 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/88899/astro-docs' }],
 			sidebar: [
 				{
-					label: 'Aiedu',
+					label: '指南',
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'About', slug: 'aiedu/about' },
 					],
 				},
 				{
-					label: 'Reference',
+					label: '接口文档',
 					autogenerate: { directory: 'reference' },
 				},
 			],
 		}),
 	],
+	shikiConfig: {
+		theme: 'dracula',
+	},
+	theme: {
+    	// 更换 Logo
+		logo: {
+		// 亮色模式 Logo（默认显示）
+		light: 'assets/light.png',
+		// 暗色模式 Logo（切换暗色主题时显示）
+		dark: 'assets/dark.png',
+		// Logo 图片的 alt 文本（必填，用于 accessibility）
+		alt: 'AiEDU | DOCS',
+	},
+  },
 });
