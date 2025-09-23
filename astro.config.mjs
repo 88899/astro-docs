129b9e68c29ebfea947933bfb286976f49977f8d
88899 <githubmen@gmail.com>
2025-09-23 13:45:49 +0800

    theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,33 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'AiEDU | DOCS',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/88899/astro-docs' }],
+			head: [
+				{
+				tag: 'script',
+				content: `
+					// 页面加载前执行
+					(() => {
+					// 读取本地存储的主题偏好（默认不存在）
+					const savedScheme = localStorage.getItem('starlight-theme');
+					// 若没有保存的偏好，强制设为亮色
+					if (!savedScheme) {
+						localStorage.setItem('starlight-theme', 'light');
+					}
+					// 应用主题（移除暗色类）
+					document.documentElement.classList.toggle(
+						'dark',
+						localStorage.getItem('starlight-theme') === 'dark'
+					);
+					})();
+				`,
+				},
+			],
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/88899/astro-docs' },
+				{ icon: 'twitter', label: 'Discord', href: 'https://x.com/home' },
 
+			],
+			favicon: '/favicon.svg',
 			sidebar: [
 				{
 					label: '指南',
@@ -26,6 +51,7 @@ export default defineConfig({
 				light: './src/assets/light.png', // 亮色模式 Logo
 				dark: './src/assets/dark.png', // 暗色模式 Logo
 				alt: 'AiEDU|DOCS', // 替代文本
+				replacesTitle: true, // 如果设置为 true，则隐藏标题文本
 
 			},
 			customCss: ['./src/style/global.css','./src/style/theme.css'],
